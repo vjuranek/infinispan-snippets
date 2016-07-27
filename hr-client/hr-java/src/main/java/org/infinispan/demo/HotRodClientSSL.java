bfe8b858b2a004e61f6b740a448dff9bca14376a
Vojtech Juranek <vjuranek@redhat.com>
2016-07-27 19:54:38 +0200

    Use diffrent passwd for keystore and key

--- hr-client/hr-java/src/main/java/org/infinispan/demo/HotRodClientSSL.java
+++ hr-client/hr-java/src/main/java/org/infinispan/demo/HotRodClientSSL.java
@@ -12,7 +12,7 @@ import org.infinispan.demo.util.SaslUtils;
 
 /**
  * Example HR client app, which use SSL only for encryption communication between client and server, and also for HR
- * client authentication, using SASL EXTERANL mechanism. See conf/sasl_exteranl_auth.xml how to configure ISPN server
+ * client authentication, using SASL EXTERANL mechanism. See conf/sasl_external_auth.xml how to configure ISPN server
  * for this use case.
  * 
  * @author vjuranek
@@ -21,23 +21,26 @@ import org.infinispan.demo.util.SaslUtils;
 public class HotRodClientSSL {
 
     public static final String ISPN_IP = "127.0.0.1";
-    public static final String SERVER_NAME = "node0";
     public static final String SASL_MECH = "EXTERNAL";
 
-    private static final String KEYSTORE_PATH = "./keystore_client.jks";
+    private static final String KEYSTORE_PATH = "./certs/keystore_client.jks";
     private static final String KEYSTORE_PASSWORD = "secret";
-    private static final String TRUSTSTORE_PATH = "./truststore_client.jks";
+    private static final String KEY_PASSWORD = "changeme";
+    private static final String TRUSTSTORE_PATH = "./certs/truststore_client.jks";
     private static final String TRUSTSTORE_PASSWORD = "secret";
 
     public static void main(String[] args) {
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.addServer().host(ISPN_IP).port(ConfigurationProperties.DEFAULT_HOTROD_PORT);
         //setup auth
-        builder.security().authentication().serverName(SERVER_NAME).saslMechanism(SASL_MECH).enable()
+        builder.security().authentication().saslMechanism(SASL_MECH).enable()
                 .callbackHandler(new SaslUtils.VoidCallbackHandler());
         //setup encrypt
-        builder.security().ssl().enable().keyStoreFileName(KEYSTORE_PATH)
-                .keyStorePassword(KEYSTORE_PASSWORD.toCharArray()).trustStoreFileName(TRUSTSTORE_PATH)
+        builder.security().ssl().enable()
+                .keyStoreFileName(KEYSTORE_PATH)
+                .keyStorePassword(KEYSTORE_PASSWORD.toCharArray())
+                .keyStoreCertificatePassword(KEY_PASSWORD.toCharArray())
+                .trustStoreFileName(TRUSTSTORE_PATH)
                 .trustStorePassword(TRUSTSTORE_PASSWORD.toCharArray());
 
         RemoteCacheManager cacheManager = new RemoteCacheManager(builder.build());
