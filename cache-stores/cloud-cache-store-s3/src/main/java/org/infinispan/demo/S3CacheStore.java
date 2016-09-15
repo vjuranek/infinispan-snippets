a66c368dbc42f03c8af610977e14e8b6a98220fb
Vojtech Juranek <vjuranek@redhat.com>
2016-09-15 23:32:20 +0200

    Create exmaple cloud cache store for AWS S3

---
+++ cache-stores/cloud-cache-store-s3/src/main/java/org/infinispan/demo/S3CacheStore.java
@@ -0,0 +1,30 @@
+package org.infinispan.demo;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.persistence.cloud.configuration.CloudStoreConfigurationBuilder;
+
+public class S3CacheStore {
+
+    public static void main(String[] args) throws Exception { 
+        ConfigurationBuilder cfg = new ConfigurationBuilder();
+        cfg.persistence().addStore(CloudStoreConfigurationBuilder.class).provider("aws-s3")
+                .endpoint("http://s3.amazonaws.com")
+                .identity("Access Key ID")
+                .credential("Secret Access Key")
+                .container("ispn-store")
+                .location("eu-central-1");
+
+        DefaultCacheManager cacheManager = new DefaultCacheManager(cfg.build());
+        Cache<String, String> cache = cacheManager.getCache("test");
+        cache.put("key", "value");
+        System.out.printf("key = %s\n", cache.get("key"));
+        
+        cache.stop();
+        cache.start();
+        
+        System.out.printf("key = %s\n", cache.get("key"));
+        cacheManager.stop();
+    }
+}
