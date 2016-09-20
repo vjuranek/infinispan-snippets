d8a2f6f84e2130a922fc6a71e15ced3da6fc39a4
Vojtech Juranek <vjuranek@redhat.com>
2016-09-20 14:45:55 +0200

    Create example for Ceph cache store

---
+++ cache-stores/ceph-cache-store/src/main/java/org/infinispan/demo/CephCacheStore.java
@@ -0,0 +1,29 @@
+package org.infinispan.demo;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.persistence.ceph.configuration.CephStoreConfigurationBuilder;
+
+public class CephCacheStore {
+
+    public static void main(String[] args) throws Exception { 
+        ConfigurationBuilder cfg = new ConfigurationBuilder();
+        cfg.persistence().addStore(CephStoreConfigurationBuilder.class)
+            .userName("admin")
+            .key("AQCY2sdXyDIcJxAAK1edRJ8xOJ2NkkiXzAuq5A==")
+            .monitorHost("192.168.122.145:6789")
+            .poolName("ispn-store");
+
+        DefaultCacheManager cacheManager = new DefaultCacheManager(cfg.build());
+        Cache<String, String> cache = cacheManager.getCache("test");
+        cache.put("key", "value");
+        System.out.printf("key = %s\n", cache.get("key"));
+        
+        cache.stop();
+        cache.start();
+        
+        System.out.printf("key = %s\n", cache.get("key"));
+        cacheManager.stop();
+    }
+}
