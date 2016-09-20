66b4333440c1d4439ccf6779c145ceaca959ed33
Vojtech Juranek <vjuranek@redhat.com>
2016-09-20 23:19:13 +0200

    Add also XML Ceph store configuration

--- cache-stores/ceph-cache-store/src/main/java/org/infinispan/demo/CephCacheStore.java
+++ cache-stores/ceph-cache-store/src/main/java/org/infinispan/demo/CephCacheStore.java
@@ -1,21 +1,21 @@
 package org.infinispan.demo;
 
 import org.infinispan.Cache;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.persistence.ceph.configuration.CephStoreConfigurationBuilder;
 
 public class CephCacheStore {
 
     public static void main(String[] args) throws Exception { 
-        ConfigurationBuilder cfg = new ConfigurationBuilder();
+        /*ConfigurationBuilder cfg = new ConfigurationBuilder();
         cfg.persistence().addStore(CephStoreConfigurationBuilder.class)
             .userName("admin")
             .key("AQCY2sdXyDIcJxAAK1edRJ8xOJ2NkkiXzAuq5A==")
             .monitorHost("192.168.122.145:6789")
             .poolName("ispn-store");
 
-        DefaultCacheManager cacheManager = new DefaultCacheManager(cfg.build());
+        DefaultCacheManager cacheManager = new DefaultCacheManager(cfg.build());*/
+        
+        DefaultCacheManager cacheManager = new DefaultCacheManager("ispn-ceph.xml");
         Cache<String, String> cache = cacheManager.getCache("test");
         cache.put("key", "value");
         System.out.printf("key = %s\n", cache.get("key"));
