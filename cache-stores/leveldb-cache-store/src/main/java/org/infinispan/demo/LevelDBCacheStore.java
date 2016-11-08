d23b25c861f1a483057f816cc598cdd3fd00dc6b
Vojtech Juranek <vjuranek@redhat.com>
2016-11-08 16:09:43 +0100

    Create simple LevelDB cachestore example

---
+++ cache-stores/leveldb-cache-store/src/main/java/org/infinispan/demo/LevelDBCacheStore.java
@@ -0,0 +1,29 @@
+package org.infinispan.demo;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfigurationBuilder;
+
+public class LevelDBCacheStore {
+
+    public static void main(String[] args) throws Exception { 
+        ConfigurationBuilder cfg = new ConfigurationBuilder();
+        cfg.persistence().addStore(LevelDBStoreConfigurationBuilder.class)
+            .location("/tmp/leveldb/")
+            .expiredLocation("/tmp/leveldb/expired/");
+        DefaultCacheManager cacheManager = new DefaultCacheManager(cfg.build());
+        Cache<String, String> cache = cacheManager.getCache("test");
+        
+        for (int i = 0; i < 100; i++) {
+            cache.put("key" + i, "value" + i);
+        }
+        System.out.printf("Cache size: %d\n", cache.size());
+        
+        cache.stop();
+        cache.start();
+        
+        System.out.printf("Cache size: %d\n", cache.size());
+        cacheManager.stop();
+    }
+}
