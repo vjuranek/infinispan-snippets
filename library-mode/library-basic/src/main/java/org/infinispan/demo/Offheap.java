be8dff313f520268ff1971715f0a7374718fec55
Vojtech Juranek <vjuranek@redhat.com>
2017-08-16 14:40:17 +0200

    Add offheap example with eviction of large entries

--- library-mode/library-basic/src/main/java/org/infinispan/demo/Offheap.java
+++ library-mode/library-basic/src/main/java/org/infinispan/demo/Offheap.java
@@ -1,5 +1,7 @@
 package org.infinispan.demo;
 
+import java.util.Random;
+
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
@@ -11,7 +13,7 @@ import org.infinispan.manager.EmbeddedCacheManager;
 public class Offheap {
 
     public static void main(String[] args) throws Exception {
-        offheapEvictionExample();
+        offheapEvictionLargeEntries();
     }
 
     public static void offheapEvictionExample() {
@@ -27,7 +29,35 @@ public class Offheap {
         dumpCache(cache);
         ecm.stop();
     }
+    
+    public static void offheapEvictionLargeEntries() {
+        Configuration conf = new ConfigurationBuilder().memory().storageType(StorageType.OFF_HEAP)
+                .evictionType(EvictionType.COUNT).size(5).build();
+        EmbeddedCacheManager ecm = new DefaultCacheManager(conf);
+        Cache<String, byte[]> cache = ecm.getCache();
+        Random rand = new Random();
+        byte[] bytes = new byte[8192000]; //8MiB
 
+        for (int i = 0; i < 100; i++) {
+            rand.nextBytes(bytes);
+            cache.put("key" + i, bytes);
+        }
+        System.out.printf("Eviction: cache size: %d\n", cache.size());
+        ecm.stop();
+    }
+
+    private static void dumpKeys(Cache<String, String> cache) {
+        System.out.println("Dumping cache keys:");
+        System.out.println("-------------------");
+        if (cache.size() == 0) {
+            System.out.println("No entries in the cache");
+        } else {
+            for (String key : cache.keySet()) {
+                System.out.printf("%s\n", key);
+            }
+        }
+    }
+    
     private static void dumpCache(Cache<String, String> cache) {
         System.out.println("Dumping cache:");
         System.out.println("--------------");
