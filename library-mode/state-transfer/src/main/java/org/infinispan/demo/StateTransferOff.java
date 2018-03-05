6f21db1e8b0da453d6a885219774aa398cb9fe9f
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-03-05 13:52:42 +0100

    Wait for cluster and remove code commnected out

--- library-mode/state-transfer/src/main/java/org/infinispan/demo/StateTransferOff.java
+++ library-mode/state-transfer/src/main/java/org/infinispan/demo/StateTransferOff.java
@@ -11,34 +11,30 @@ import org.infinispan.manager.EmbeddedCacheManager;
 
 public class StateTransferOff {
 
+    private static final int CLUSTER_WAIT = 10;
+
     public static void main(String[] args) throws Exception {
         testStateTransferOff();
     }
 
     public static void testStateTransferOff() {
         GlobalConfiguration gc = GlobalConfigurationBuilder.defaultClusteredBuilder().clusteredDefault().build();
-        /*ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.clustering().cacheMode(CacheMode.REPL_SYNC)
-            .stateTransfer().fetchInMemoryState(true).awaitInitialTransfer(true);
-        EmbeddedCacheManager ecm = new DefaultCacheManager(gc, cb.build());*/
         EmbeddedCacheManager ecm = new DefaultCacheManager(gc, (new ConfigurationBuilder()).build());
-        
-        Configuration cacheConf = new ConfigurationBuilder().clustering()
-                .cacheMode(CacheMode.REPL_SYNC)
-                .stateTransfer().fetchInMemoryState(false).awaitInitialTransfer(false).build();
+
+        Configuration cacheConf = new ConfigurationBuilder().clustering().cacheMode(CacheMode.REPL_SYNC).stateTransfer()
+                .fetchInMemoryState(true).awaitInitialTransfer(false).build();
         ecm.defineConfiguration("testCache", cacheConf);
         Cache<String, String> cache = ecm.getCache("testCache");
-        
-        //if (ecm.getTransport().getMembers().size() == 1) {
+
         if (ecm.getTransport().getCoordinator().equals(ecm.getTransport().getAddress())) {
             populateCache(cache);
         }
         
+        waitForCluster(ecm);
         dumpCache(cache);
         ecm.stop();
     }
-    
-    
+
     private static void populateCache(Cache<String, String> cache) {
         System.out.println("Populating cache " + cache.getCacheManager().getTransport().getAddress());
         for (int i = 0; i < 10; i++) {
@@ -46,13 +42,8 @@ public class StateTransferOff {
         }
         dumpCache(cache);
         System.out.println("Cache populated, now sleeping for 10 sec., start another node now!");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
-    
+
     private static void dumpCache(Cache<String, String> cache) {
         System.out.println("Dumping cache (" + cache.getCacheManager().getTransport().getAddress() + "):");
         System.out.println("Cache size: " + cache.size());
@@ -62,6 +53,24 @@ public class StateTransferOff {
         } else {
             for (String key : cache.keySet()) {
                 System.out.printf("%s -> %s\n", key, cache.get(key));
+            }
+        }
+    }
+
+    private static void waitForCluster(EmbeddedCacheManager ecm) {
+        for (int i = 0; i < CLUSTER_WAIT; i++) {
+            boolean isClusterFormed = ecm.getTransport().getMembers().size() > 1;
+            //always sleep at least once so that if the cluster is formed, another node knows about it as well
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (isClusterFormed) {
+                System.out.println("Cluster formed!");
+                break;
+            } else {
+                System.out.println("Waiting for cluster another 10 sec.");
             }
         }
     }
